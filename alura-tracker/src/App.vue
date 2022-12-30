7e6e5c7ec017a280934d301875766e5c44bae6cb
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-12-30 17:20:42 -0300

    projeto iniciado

---
+++ alura-tracker/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+
+    </div>
+
+    <div class="column is-one-quarter">
+    </div>
+
+
+
+  </main>
+ 
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+
+</style>
