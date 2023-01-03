2c4015e55de3e2631409554c479edb00aa01ffd7
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2023-01-03 15:02:41 -0300

    criados os componentes formulario e barra lateral

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
+      <BarraLateral/>
 
     </div>
 
-    <div class="column is-one-quarter">
+    <div class="column is-three-quarter">
+      <Formulario/>
     </div>
 
 
@@ -15,10 +17,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue'
+import Formulario from './components/Formulario.vue'
 
 
 export default defineComponent({
   name: 'App',
+  components:{
+    BarraLateral,
+    Formulario
+  }
 });
 </script>
 
